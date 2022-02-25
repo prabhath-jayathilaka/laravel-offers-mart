797427603deada98529346084ac30fb90388f4c8
codeJay <90417125+prabhath-jayathilaka@users.noreply.github.com>
2022-02-25 19:52:14 +0530

    restructure db orders schema adding 'no_of_items','status', 'order_date', 'deliver_date','return_date' ,  'return_reason' columns. restructure db orders schema adding 'no_of_items','status', 'order_date', 'deliver_date','return_date' ,  'return_reason' columns.

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -18,9 +18,12 @@ class OrderFactory extends Factory
     public function definition()
     {
         $suffixes = ['Sweater', 'Pants', 'Shirt', 'Hat', 'Jeans', 'Socks'];
+        $status = Arr::random(['PROCESSING', 'DELIVERED', 'DISPATCHED', 'RETURNING']);
         return [
             'transaction_id' => $this->faker->slug(2),
             'total' => $this->faker->numberBetween(20000, 1000000),
+            'no_of_items' => $this->faker->numberBetween(1, 10),
+            'status' => $status,
             'item_list' => ['product_id' => $this->faker->asciify(),
                 'product_name' => $this->faker->company() . ' ' . Arr::random($suffixes),
                 'qty' => $this->faker->numberBetween(2, 5),
@@ -29,6 +32,10 @@ class OrderFactory extends Factory
                 'city' => $this->faker->city(),
                 'state' => $this->faker->state(),
                 'zip_code' => $this->faker->postcode()],
+            'order_date' => $this->faker->date,
+            'deliver_date' => $status !== 'PROCESSING' ? $this->faker->date : null,
+            'return_date' => $status === 'DISPATCHED' ? $this->faker->date : null,
+            'return_reason' => $status === 'DISPATCHED' ? $this->faker->sentence : null,
         ];
     }
 }
--- database/migrations/2022_02_12_165552_create_orders_table.php
+++ database/migrations/2022_02_12_165552_create_orders_table.php
@@ -17,8 +17,14 @@ return new class extends Migration {
             $table->bigInteger('user_id');
             $table->string('transaction_id');
             $table->integer('total');
+            $table->integer('no_of_items');
+            $table->string('status');
             $table->json('item_list');
             $table->json('shipping_address');
+            $table->date('order_date');
+            $table->date('deliver_date')->nullable();
+            $table->date('return_date')->nullable();
+            $table->string('return_reason')->nullable()->default('null');
             $table->timestamps();
         });
     }
