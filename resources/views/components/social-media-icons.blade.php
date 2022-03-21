8ff2ba101e44e60bd04c77b4c34cdb98d59b9022
codeJay <90417125+prabhath-jayathilaka@users.noreply.github.com>
2022-03-22 04:24:04 +0530

    logo added

---
+++ resources/views/components/social-media-icons.blade.php
@@ -0,0 +1,13 @@
+<ul class="w-48 flex justify-between">
+    <li><a href="https://www.facebook.com/"> <img class="w-6 h-6" src="{{ asset('/storage/media/icons/facebook_icon.png') }}">
+        </a></li>
+    <li><a href="https://www.facebook.com/"> <img class="w-6 h-6" src="{{ asset('/storage/media/icons/instagram_icon.png') }}">
+        </a></li>
+    <li><a href="https://www.facebook.com/"> <img class="w-6 h-6" src="{{ asset('/storage/media/icons/pinterest_icon.png') }}">
+        </a></li>
+    <li><a href="https://www.facebook.com/"> <img class="w-6 h-6" src="{{ asset('/storage/media/icons/reddit_icon.png') }}">
+        </a></li>
+    <li><a href="https://www.facebook.com/"> <img class="w-6 h-6" src="{{ asset('/storage/media/icons/tumblr_icon.png') }}">
+        </a>
+    </li>
+</ul>
